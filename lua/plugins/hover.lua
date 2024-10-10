838015f3473bdf7db9e89de66fd5d9d7f71e917f
Angju <154499065+timeless-spell@users.noreply.github.com>
2024-10-10 22:15:52 +0000

    Fixed config

--- lua/plugins/hover.lua
+++ lua/plugins/hover.lua
@@ -1,38 +1,49 @@
 return {
 	"lewis6991/hover.nvim",
-	config = function()
-		require("hover").setup({
-			init = function()
-				require("hover.providers.lsp")
-				-- require("hover.providers.dap")
-				require("hover.providers.diagnostic")
-				require("hover.providers.man")
-			end,
-			preview_opts = {
-				border = "single",
+	keys = function(_, keys)
+		local hover = require("hover")
+		local keys = {
+			{
+				"<leader>K",
+				function()
+					hover.hover()
+				end,
+				desc = "Hover",
 			},
-			-- whether the contents of a currently open hover window should be moved
-			-- to a :h preview-window when pressing the hover keymap.
-			preview_window = false,
-			title = true,
-			mouse_providers = {
-				"lsp",
+			{
+				"<leader>gK",
+				function()
+					hover.hover_select()
+				end,
+				desc = "Hover Select",
 			},
-			mouse_delay = 1000,
-		})
-
-		-- setup keymaps
-		vim.keymap.set("n", "<leader>k", require("hover").hover, { desc = "hover" })
-		vim.keymap.set("n", "<leader>gk", require("hover").hover_select, { desc = "hover (select)" })
-		vim.keymap.set("n", "<c-p>", function()
-			require("hover").hover_switch("previous")
-		end, { desc = "hover.nvim (previous source)" })
-		vim.keymap.set("n", "<c-n>", function()
-			require("hover").hover_switch("next")
-		end, { desc = "hover.nvim (next source)" })
-
-		-- mouse support
-		vim.keymap.set("n", "<mousemove>", require("hover").hover_mouse, { desc = "hover.nvim (mouse)" })
-		vim.opt.mousemoveevent = true
+			{
+				"<mousemove>",
+				function()
+					hover.hover_mouse()
+				end,
+				desc = "Mouse Hover",
+			},
+		}
+		return keys
 	end,
+	opts = {
+		init = function()
+			require("hover.providers.lsp")
+			require("hover.providers.dap")
+			require("hover.providers.diagnostic")
+			require("hover.providers.man")
+			require("hover.providers.gh")
+			require("hover.providers.gh_user")
+		end,
+		preview_opts = {
+			border = "single",
+		},
+		mouse_providers = {
+			"lsp",
+		},
+		mouse_delay = 1000,
+		preview_window = false,
+		title = true,
+	},
 }
