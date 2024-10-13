940ed8a47d26e102c5bd66961426478a70a6605a
Angju <154499065+timeless-spell@users.noreply.github.com>
2024-10-13 17:56:54 +0000

    Bug fix

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -45,7 +45,7 @@ return {
 			-- online, please don't ask me how to install them :)
 			ensure_installed = {
 				-- Update this to ensure that you have the debuggers for the langs you want
-				"delve",
+				-- "delve",
 			},
 		})
 
@@ -55,20 +55,20 @@ return {
 			-- Set icons to characters that are more likely to work in every terminal.
 			--    Feel free to remove or use ones that you like more! :)
 			--    Don't feel like these are good choices.
-			icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
-			controls = {
-				icons = {
-					pause = "⏸",
-					play = "▶",
-					step_into = "⏎",
-					step_over = "⏭",
-					step_out = "⏮",
-					step_back = "b",
-					run_last = "▶▶",
-					terminate = "⏹",
-					disconnect = "⏏",
-				},
-			},
+			-- icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+			-- controls = {
+				-- icons = {
+				-- 	pause = "⏸",
+				-- 	play = "▶",
+				-- 	step_into = "⏎",
+				-- 	step_over = "⏭",
+				-- 	step_out = "⏮",
+				-- 	step_back = "b",
+				-- 	run_last = "▶▶",
+				-- 	terminate = "⏹",
+				-- 	disconnect = "⏏",
+				-- },
+			-- },
 		})
 
 		dap.listeners.after.event_initialized["dapui_config"] = dapui.open
