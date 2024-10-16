13e410f6226e59ad506ca1ea0e19b87f0c447f6a
Angju <154499065+timeless-spell@users.noreply.github.com>
2024-10-16 13:03:14 +0000

    Fix

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -19,17 +19,12 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 			},
-			-- format_on_save = function(bufnr)
-			-- 	-- Disable "format_on_save lsp_fallback" for languages that don't
-			-- 	-- have a well standardized coding style. You can add additional
-			-- 	-- languages here or re-enable it for the disabled ones.
-			-- 	local disable_filetypes = { c = false, cpp = false }
-			-- 	return {
-			-- 		timeout_ms = 2500,
-			-- 		lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-			-- 	}
-			-- end,
-			-- notify_on_error = false,
+			default_format_opts = {
+				lsp_format = "fallback",
+			},
+			format_after_save = {
+				lsp_format = "fallback",
+			},
 		})
 
 		vim.api.nvim_create_user_command("Format", function(args)
