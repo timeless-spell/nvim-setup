82f0976232bec1ff33b882b53e0d5e005f979e8a
Angju <154499065+timeless-spell@users.noreply.github.com>
2025-01-24 04:49:55 -0400

    Added nvim-highlight-colors plugin support.

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -12,6 +12,7 @@ return {
             "moyiz/blink-emoji.nvim",
             "mtoohey31/cmp-fish",
             "chrisgrieser/cmp-nerdfont",
+            "brenoprata10/nvim-highlight-colors",
         },
         version = "*",
         opts = {
@@ -30,18 +31,36 @@ return {
                         columns = {
                             { "label", "label_description", gap = 1 },
                             { "kind_icon", "kind", gap = 1 },
-                            { "source_name" },
+                            { "source_name", gap = 1 },
                         },
                         components = {
                             kind_icon = {
                                 ellipsis = false,
                                 text = function(ctx)
-                                    local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-                                    return kind_icon
+                                    local icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                                    if ctx.item.source_name == "LSP" then
+                                        local color_item = require("nvim-highlight-colors").format(
+                                            ctx.item.documentation,
+                                            { kind = ctx.kind }
+                                        )
+                                        if color_item and color_item.abbr then
+                                            icon = icon .. " " .. color_item.abbr
+                                        end
+                                    end
+                                    return icon .. ctx.icon_gap
                                 end,
                                 highlight = function(ctx)
-                                    local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-                                    return hl
+                                    local _, highlight, _ = require("mini.icons").get("lsp", ctx.kind)
+                                    if ctx.item.source_name == "LSP" then
+                                        local color_item = require("nvim-highlight-colors").format(
+                                            ctx.item.documentation,
+                                            { kind = ctx.kind }
+                                        )
+                                        if color_item and color_item.abbr_hl_group then
+                                            highlight = color_item.abbr_hl_group
+                                        end
+                                    end
+                                    return highlight
                                 end,
                             },
                         },
