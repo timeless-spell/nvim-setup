3e772a7de1b4e6b40080877cef91b78f9e8376e0
Angju <154499065+timeless-spell@users.noreply.github.com>
2025-02-05 11:26:34 -0400

    Formatted and cleaned keymaps.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,4 @@
 local map = vim.keymap.set
-local dmap = vim.keymap.del
 
 -- Close current buffer
 map('n', '<a-d>', '<CMD>bdelete<CR>', { desc = 'Close current bdeletebuffer' })
@@ -20,44 +19,14 @@ map('n', '<c-j>', '<c-w><c-j>', { desc = 'Move focus to the lower window' })
 map('n', '<c-k>', '<c-w><c-k>', { desc = 'Move focus to the upper window' })
 
 -- Naviagate buffers
-map(
-  'n',
-  '<a-a>',
-  '<CMD>bnext<cr>',
-  { noremap = true, silent = true, desc = 'Navigate next buffer' }
-)
-map(
-  'n',
-  '<a-s>',
-  '<CMD>bprevious<cr>',
-  { noremap = true, silent = true, desc = 'Navigate previous buffer' }
-)
+map('n', '<a-a>', '<CMD>bnext<cr>', { noremap = true, silent = true, desc = 'Navigate next buffer' })
+map('n', '<a-s>', '<CMD>bprevious<cr>', { noremap = true, silent = true, desc = 'Navigate previous buffer' })
 
 -- Modify Split Size
-map(
-  'n',
-  '<c-up>',
-  '<CMD>resize -2<CR><cr>',
-  { noremap = true, silent = true, desc = 'Resize split Up' }
-)
-map(
-  'n',
-  '<c-down>',
-  '<CMD>resize +2<CR><cr>',
-  { noremap = true, silent = true, desc = 'Resize split Down' }
-)
-map(
-  'n',
-  '<c-left>',
-  '<CMD>vertical resize -2<cr>',
-  { noremap = true, silent = true, desc = 'Resize split Left' }
-)
-map(
-  'n',
-  '<c-right>',
-  '<CMD>vertical resize +2<cr>',
-  { noremap = true, silent = true, desc = 'Resize split Right' }
-)
+map('n', '<c-up>', '<CMD>resize -2<CR><cr>', { noremap = true, silent = true, desc = 'Resize split Up' })
+map('n', '<c-down>', '<CMD>resize +2<CR><cr>', { noremap = true, silent = true, desc = 'Resize split Down' })
+map('n', '<c-left>', '<CMD>vertical resize -2<cr>', { noremap = true, silent = true, desc = 'Resize split Left' })
+map('n', '<c-right>', '<CMD>vertical resize +2<cr>', { noremap = true, silent = true, desc = 'Resize split Right' })
 
 -- [[ Diagnostic keymaps ]]
 map('n', '<leader>ll', vim.diagnostic.setloclist, { desc = 'Open diagnostic [L]ocation list' })
@@ -65,15 +34,3 @@ map('n', '<leader>lq', vim.diagnostic.setqflist, { desc = 'Open diagnostic [Q]ui
 map('n', '<leader>le', vim.diagnostic.open_float, { desc = 'Open Floating diagnostic ' })
 map('n', '<leader>ln', vim.diagnostic.get_next, { desc = 'Open diagnostic Next Diagnostic' })
 map('n', '<leader>lp', vim.diagnostic.get_prev, { desc = 'Open diagnostic Previous Diagnostic' })
-
--- local v = vim.version.parse(vim.system({ "nvim", "--version" }, { text = true }):wait().stdout)
--- if v == "0.10.*" then
--- 	return
--- else
--- 	-- Deleted default keymaps to avoid duplicates
--- 	dmap({ "n", "x" }, "gra")
--- 	dmap("n", "grr")
--- 	dmap("n", "grn")
--- 	dmap("i", "<c-s>")
--- 	dmap("n", "gri")
--- end
