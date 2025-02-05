4c677a092dbd0c7c0025889eb6d440a5c10dcd3b
Angju <154499065+timeless-spell@users.noreply.github.com>
2025-02-05 15:22:39 -0400

    New plugin: nvim-lint.

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,21 @@
+return {
+  'mfussenegger/nvim-lint',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local lint = require('lint')
+    lint.linters_by_ft = {
+      lua = { 'selene' },
+      fish = { 'fish' },
+    }
+
+    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost' }, {
+      group = lint_augroup,
+      callback = function()
+        if vim.opt_local.modifiable:get() then
+          lint.try_lint()
+        end
+      end,
+    })
+  end,
+}
