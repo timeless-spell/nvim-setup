c4f34cb23eaf99a32db23e4f1d06af04554164df
Angju <154499065+timeless-spell@users.noreply.github.com>
2025-02-05 11:33:14 -0400

    Formatted.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,25 +1,25 @@
 return {
-	"folke/snacks.nvim",
-	priority = 1000,
-	lazy = false,
-	opts = function()
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "MiniFilesActionRename",
-			callback = function(event)
-				Snacks.rename.on_rename_file(event.data.from, event.data.to)
-			end,
-		})
-		return {
-			bigfile = { enabled = true },
-			dashboard = { enabled = false },
-			indent = { enabled = false },
-			input = { enabled = true },
-			picker = { enabled = false },
-			notifier = { enabled = true },
-			quickfile = { enabled = true },
-			scroll = { enabled = true },
-			statuscolumn = { enabled = true },
-			words = { enabled = true },
-		}
-	end,
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  opts = function()
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MiniFilesActionRename',
+      callback = function(event)
+        Snacks.rename.on_rename_file(event.data.from, event.data.to)
+      end,
+    })
+    return {
+      bigfile = { enabled = true },
+      dashboard = { enabled = false },
+      indent = { enabled = false },
+      input = { enabled = true },
+      picker = { enabled = false },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    }
+  end,
 }
